7a88390244bdfedd533adb1720001f1efb1b37a8
mugu <60588846+yinmincc@users.noreply.github.com>
2026-03-23 22:07:37 +0100

    Create day17_SubscriptionLogicV1.sol

---
+++ Kathy/day17_SubscriptionLogicV1.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./day17_SubscriptionStorageLayout.sol";
+
+contract SubscriptionLogicV1 is SubscriptionStorageLayout {
+    function addPlan(uint8 planId, uint256 price, uint256 duration) external {
+        planPrices[planId] = price;
+        planDuration[planId] = duration;
+    }
+
+    function subscribe(uint8 planId) external payable {
+        require(planPrices[planId] > 0, "Invalid plan");
+        require(msg.value >= planPrices[planId], "Insufficient payment");
+
+        Subscription storage s = subscriptions[msg.sender];
+        if (block.timestamp < s.expiry) {
+            s.expiry += planDuration[planId];
+        } else {
+            s.expiry = block.timestamp + planDuration[planId];
+        }
+
+        s.planId = planId;
+        s.paused = false;
+    }
+
+    function isActive(address user) external view returns (bool) {
+        Subscription memory s = subscriptions[user];
+        return (block.timestamp < s.expiry && !s.paused);
+    }
+}
+
